15a6eb05b34c76d9cc95ce5f48741085c9224a42
taher33 <taherhehe@gmail.com>
2021-10-19 21:58:28 +0100

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primaryColor: "#2083F8",
+    primaryColorHover: "#7CA5D4",
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
